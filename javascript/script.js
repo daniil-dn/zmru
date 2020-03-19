a9140a04da6f7c41ba08a2b12560c47f72027cb5
Daniil <pro777dan@gmail.com>
2020-03-19 23:04:43 +0300

    I added menu for narrow screen/ work stable but there is a bug and there is not any animations

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,14 @@
 $(".button").on("click", function (event) {
     event.preventDefault();
    $(this).toggleClass("toggled");
+   if ($(this).hasClass('toggled')){
+       $(".but_narrow_wrap").css({"border-radius": ' 10px 10px 0 0 ',});
+       $('.narrow_buttons').removeClass('disp_none')
+   }else{
+       $(".but_narrow_wrap").css({"border-radius": ' 10px',});
+
+       $('.narrow_buttons').addClass('disp_none')
+   }
+
 });
 
