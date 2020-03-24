25ef93dd9ce152151c398767b0c0e9aef1ed3334
Daniil <pro777dan@gmail.com>
2020-03-24 21:59:53 +0300

    stable animation of menu/ I have to refactor the code!!!

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,34 @@
+var but_narrow_tmLite = new TimelineMax();
+
 
 $(".button").on("click", function (event) {
     event.preventDefault();
+
     $(this).toggleClass("toggled");
     if ($(this).hasClass('toggled')) {
-        $(".but_narrow_wrap").css({"border-radius": ' 10px 10px 0 0 ', 'background': '#d6d6d6'});
-        $('.narrow_buttons').removeClass('disp_none')
+        $('.narrow_buttons').removeClass('disp_none');
+        but_narrow_tmLite.to('.but_narrow_wrap', 0.1, {
+            backgroundColor: 'rgb(212, 212, 212)'
+        }).to('.but_narrow_wrap', 0.2, {borderRadius: '10px 10px 0 0'},)
+            .to('.narrow_buttons', 0.2, {height: 'auto'}, '+=0.2')
+            .to('.narrow_buttons div', 0.2, {transform: 'translateY(0)'})
+            .to('.narrow_buttons div', 0.2, {opacity: 1}, "-=0.3")
+
+        ;
+
+
+        if (but_narrow_tmLite.progress() != 1) {
+            //carl just changed this again
+            but_narrow_tmLite.play();
+        } else {
+            but_narrow_tmLite.restart();
+        }
+
     } else {
-
-        $(".but_narrow_wrap").css({"border-radius": ' 10px', 'background': 'none'});
-        $('.narrow_buttons').addClass('disp_none')
-
-
+        but_narrow_tmLite.reverse(1)
+        setTimeout(() => {
+            $('.narrow_buttons').addClass('disp_none')
+        }, 1000)
     }
-
 });
-console.log(gsap.version)
-import validate_form from './form_validator.mjs'
-validate_form({
-    formId: 'profile',
-    formValidClass: 'form_valid',
-    formInvalidClass: 'form_invalid',
-
-    inputErrorClass: 'input_error'
-});
+// console.log(gsap.version)
